86e76019ea73eab6435b869b1336e8ac37034c54
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-01-06 01:18:22 +0100

    Fix internet connection widget

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
         <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a24"></twitter>
         <time-weather position="b1:b8" date-format="ddd DD MMM" time-format="hh:mm A" time-zone="Europe/Madrid" weather-city="Oviedo" time-format="h:mm:ss a"></time-weather>
         <fitness position="c1:c8"></fitness>
-        <internet-connection position="b1:c8"></internet-connection>
+        <internet-connection position="b1:b8"></internet-connection>
         <task-list position="b9:c16"></task-list>
         <music position="b17:d24"></music>
         <mrr position="d1:d8"></mrr>
