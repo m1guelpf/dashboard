a23f0aa8f1750d0746b79ada4c54658d336650af
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-01-08 17:22:31 +0100

    Fix sleep fetch command

--- app/Console/Components/Google/FetchSleepDataCommand.php
+++ app/Console/Components/Google/FetchSleepDataCommand.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Components\Google;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Console\Command;
 use App\Events\Fitness\SleepFetched;
-use Carbon\Carbon;
 use Google_Service_Fitness_Session as Session;
 
 class FetchSleepDataCommand extends Command
@@ -23,16 +23,13 @@ class FetchSleepDataCommand extends Command
 
         $client = new \Google_Service_Fitness($client);
 
-        $response = $client->users_sessions->listUsersSessions('me', [
-            // 'startTime' => today()->addHours(-12),
-            // 'endTime' => today()->tomorrow()->addHours(-12),
-        ]);
+        $response = $client->users_sessions->listUsersSessions('me');
 
         $duration = collect($response->getSession())->filter(function (Session $session) {
             return $session->getActivityType() === 72;
         })->filter(function (Session $session) {
             return Carbon::parse($session->getEndTimeMillis() / 1000)->isToday();
-        })->map(function(Session $session) {
+        })->map(function (Session $session) {
             return ($session->getEndTimeMillis() - $session->getStartTimeMillis()) / 1000;
         })->first() ?? 0;
 
